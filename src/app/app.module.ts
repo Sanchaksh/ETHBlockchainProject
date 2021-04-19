f063d896bd80659052c2825f65e6f936b6feea12
sanchakshkaul <sanchakshkaul@gmail.com>
2021-04-20 00:42:32 +0530

    Added Images and better formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
+import { PollComponent } from './poll/poll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PollCreateComponent
+    PollCreateComponent,
+    PollComponent
   ],
   imports: [
     BrowserModule,
