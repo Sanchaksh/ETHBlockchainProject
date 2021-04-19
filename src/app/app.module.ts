bc623c0f36479a23eedc6622a5d71382784134cf
sanchakshkaul <sanchakshkaul@gmail.com>
2021-04-20 00:18:05 +0530

    Improved Front_end Funtionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,9 @@ import { PollCreateComponent } from './poll-create/poll-create.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/poll-create/poll-create.component.ts
+++ src/app/poll-create/poll-create.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-poll-create',
   templateUrl: './poll-create.component.html',
   styleUrls: ['./poll-create.component.scss']
 })
-export class PollCreateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+export class PollCreateComponent {
+  pollForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.pollForm = this.fb.group({
+      question: this.fb.control('', [Validators.required]),
+      image: this.fb.control(''),
+      op1: this.fb.control(''),
+      op2: this.fb.control(''),
+      op3: this.fb.control('')
+    });
   }
 
+  submitForm() {
+    console.log(this.pollForm.value);
+  }
 }
